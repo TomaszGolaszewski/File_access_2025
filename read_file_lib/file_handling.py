25320e8c6cab5695c65275398b8d9a81650d2576
Tomasz Golaszewski <tg@myodoo.pl>
2025-04-14 12:29:31 +0200

    Commit_v2

--- read_file_lib/file_handling.py
+++ read_file_lib/file_handling.py
@@ -14,10 +14,10 @@ class FileWithConstantWidth():
         ])
         self.transaction = Line(2, [
             (1, 2, "Field ID", int),
-            (3, 8, "Counter"),
+            (3, 8, "Counter", int),
             (9, 20, "Amount", int),
             (21, 23, "Currency"),
-            (24, 120, "Reserved"),
+            (24, 120, "Reserved", False),
         ])
         self.footer = Line(3, [
             (1, 2, "Field ID", int),
@@ -27,7 +27,9 @@ class FileWithConstantWidth():
         ])
         print("File init")
 
-        print(self.insert_value("header", "Name", "Tomasz", 1))
+        # print(self.insert_value("header", "Name", "Tomasz", 1))
+        print(self.add_transaction(323.4567,  "PLN"))
+        # print(self.get_value("transaction", "Amount", 3))
         try:
             with open(self.path, "r") as file:
                 pass
@@ -51,7 +53,7 @@ class FileWithConstantWidth():
         try:
             with open(self.path, "r") as file:
                 for line in file:
-                    if block_id == line[0:2] and self.transaction_check(block, line):
+                    if block_id == line[0:2] and self.transaction_check(block, line, transaction_no):
                         return getattr(self, block).get_field_value_from_line(field, line)
                 return f"NO SUCH LINE {block}"
         except FileNotFoundError:
@@ -61,13 +63,18 @@ class FileWithConstantWidth():
         if not hasattr(self, block):
             raise NameError("Wrong block name!")
             # return f"NO SUCH LINE {block}"
+        if block == "transaction":
+            # TODO: possible implementation of changes in transactions in the future
+            return "YOU CAN'T CHANGE TRANSACTIONS' HISTORY!"
+        if block == "footer":
+            return "YOU CAN'T CHANGE FOOTER!"
         block_id = getattr(self, block).id
         new_file_content = ""
         try:
             with open(self.path, "r") as file:
                 for line in file:
-                    if block_id == line[0:2] and self.transaction_check(block, line):
-                        new_file_content += getattr(self, block).insert_field_value_to_line(field, line, value)# + "\n"
+                    if block_id == line[0:2] and self.transaction_check(block, line, transaction_no):
+                        new_file_content += getattr(self, block).insert_field_value_to_line(field, line, value)
                         # print(getattr(self, block).get_field_value_from_line(field, line))
                         # return f"VALUE {value} INSERTED :)"
                     else:
@@ -77,10 +84,47 @@ class FileWithConstantWidth():
             return f"NO SUCH FILE {self.path}"
         
         self.drop_payload_to_file(new_file_content)
+
+    def add_transaction(self, amount_float: float, currency: str):
+        if amount_float < 0:
+            raise ValueError("Amount can't be negative!")
+        if currency not in ["PLN", "EUR", "USD"]:
+            raise ValueError("Wrong currency!")
+        format_amount = lambda x: int(x * 100) # convert float into int with decimals
+        amount = format_amount(amount_float)
+        new_file_content = ""
+        amount_sum = amount
+        counter = 0
+        try:
+            with open(self.path, "r") as file:
+                for line in file:
+                    if line[0:2] == "01": # header
+                        new_file_content += line # copy
+                    elif line[0:2] == "02": # transactions
+                        counter = self.transaction.get_field_value_from_line("Counter", line) # get counter
+                        amount_sum += self.transaction.get_field_value_from_line("Amount", line) # sum total amount
+                        new_file_content += line # copy
+                    elif line[0:2] == "03": # footer - insert new transaction and create new footer
+                        # add new transaction
+                        new_line = self.transaction.create_empty_line()
+                        new_line = self.transaction.insert_field_value_to_line("Counter", new_line, counter+1)
+                        new_line = self.transaction.insert_field_value_to_line("Amount", new_line, amount)
+                        new_line = self.transaction.insert_field_value_to_line("Currency", new_line, currency)
+                        new_file_content += new_line
+                        # create new footer
+                        new_line = self.footer.create_empty_line()
+                        new_line = self.footer.insert_field_value_to_line("Total Counter", new_line, counter+1)
+                        new_line = self.footer.insert_field_value_to_line("Control sum", new_line, amount_sum)
+                        new_file_content += new_line
+
+        except FileNotFoundError:
+            return f"NO SUCH FILE {self.path}"
+        
+        self.drop_payload_to_file(new_file_content)
         
     def transaction_check(self, block_name, line, transaction_no=0):
         if block_name == "transaction":
-            counter = str(self.transaction.get_field_value_from_line("Counter", line))
+            counter = int(self.transaction.get_field_value_from_line("Counter", line))
             if counter != transaction_no: return False
         return True
     
@@ -90,12 +134,8 @@ class FileWithConstantWidth():
 
     def create_empty_file(self):
         print("create_empty_file")
-        new_file_content = self.header.create_empty_line() + "\n" + \
-                    self.footer.create_empty_line() + "\n"
+        new_file_content = self.header.create_empty_line() + self.footer.create_empty_line()
         self.drop_payload_to_file(new_file_content)
-        # with open(self.path, "w") as file:
-        #     file.write(self.header.create_empty_line() + "\n")
-        #     file.write(self.footer.create_empty_line() + "\n")
 
 
 # ====== Lines ======================================================
@@ -112,7 +152,7 @@ class Line():
         result = ""
         for field_name in self.fields_list:
             result += self.fields_dict[field_name].value
-        return result
+        return result + "\n"
 
     def get_field_value_from_line(self, field_name, line):
         if not self.fields_dict.get(field_name): return f"NO SUCH FIELD {field_name}"
@@ -138,15 +178,15 @@ class Field():
         elif data_type == int:
             self.whitespace = "0"
         else:
-            self.whitespace = " "
+            self.whitespace = "."
         self.update_value(value)
 
     # def __str__(self) -> str:
     def get_placeholder(self):
         if hasattr(self, "value"):
             return self.value
-        # return  self.whitespace * self.length
-        return self.name[0] * self.length
+        return  self.whitespace * self.length
+        # return self.name[0] * self.length
 
     def check_and_format_value(self, value):
         if not value:
@@ -167,11 +207,11 @@ class Field():
     def get_value_from_line(self, line: str) -> str:
         if len(line) != 121: # 120 + end of line
             raise ImportError
-        return line[self.position_from-1: self.position_to]
+        return self.data_type(line[self.position_from-1: self.position_to])
 
     def insert_value_to_line(self, line: str, value) -> str:     
         if len(line) != 121: # 120 + end of line
-            raise ImportError
+            raise ImportError(f"Wrong line lenght, is: {len(line)}, line: {line}")
         self.value = self.check_and_format_value(value)
         return line[:self.position_from-1] + self.value + line[self.position_to:]
 
